330dfff626bfb29951da0c88bb05d6818658b632
Gianluca Aguzzi <gianluca.aguzzi@studio.unibo.it>
2026-05-26 17:29:28 +0200

    update movement to be more fluent

--- aggregate-runtime/src/main/scala/it/unibo/demo/scenarios/Program.scala
+++ aggregate-runtime/src/main/scala/it/unibo/demo/scenarios/Program.scala
@@ -8,7 +8,7 @@ class Program extends AggregateProgram with StandardSensors with BlockG:
   def module(position: (Double, Double)): Double = Math.sqrt(position._1 * position._1 + position._2 * position._2)
   def normalize(position: (Double, Double)): (Double, Double) = {
     val module = this.module(position)
-    (position._1 / module, position._2 / module)
+    if (module < 1e-9) (0.0, 0.0) else (position._1 / module, position._2 / module)
   }
   def rotate90(position: (Double, Double)): (Double, Double) = (-position._2, position._1)
   /**
